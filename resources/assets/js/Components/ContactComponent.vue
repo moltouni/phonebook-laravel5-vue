d9883e4975395dfced6de1984d48372c1ec59722
Mario <marionovak5@gmail.com>
2020-03-14 18:22:45 +0100

    Contact Component: css refactored

--- resources/assets/js/Components/ContactComponent.vue
+++ resources/assets/js/Components/ContactComponent.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="contact">
+    <!-- Controls (favorite, delete) -->
+
     <div class="controls">
       <div @click="favoriteToggle()" class="favorite">
         <icon v-if="!contact.favorite" color="#aaa" name="regular/heart"></icon>
@@ -9,6 +11,8 @@
         <icon color="#aaa" name="trash-alt"></icon>
       </div>
     </div>
+
+    <!-- Avatar -->
 
     <router-link :to="{ name: 'contact-single', params: { contact: contact.id } }">
       <avatar
@@ -20,8 +24,10 @@
       ></avatar>
     </router-link>
 
+    <!-- Name -->
+
     <router-link :to="{ name: 'contact-single', params: { contact: contact.id } }">
-      <h2 class="text">{{ contact.first_name }} {{contact.last_name}}</h2>
+      <h2 class="name">{{ contact.first_name }} {{contact.last_name}}</h2>
     </router-link>
   </div>
 </template>
@@ -31,6 +37,7 @@ import Avatar from "vue-avatar";
 
 export default {
   name: "contact-component",
+
   components: {
     Avatar
   },
@@ -54,47 +61,45 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-$margin: 64px;
 $width: 420px;
 $height: $width/1.7;
 
 .contact {
   display: inline-block;
+  position: relative;
+  max-width: 100%;
+  width: $width;
+  height: $height;
+  margin: 8px;
+
+  padding: 64px;
   overflow: hidden;
   text-align: center;
   background: #eee;
   border: 3px solid #ddd;
   border-radius: 8px;
-  padding: $margin;
-  max-width: 100%;
-  width: $width;
-  height: $height;
-  position: relative;
-  margin: 8px;
 
   .controls {
     position: absolute;
     top: 0;
-    width: 100%;
     left: 0;
-    text-align: right;
+    width: 100%;
+
     padding-right: 8px;
     padding-top: 8px;
-    div {
-      display: inline-block;
-      font-size: 16px;
-      margin: 8px;
-    }
+    text-align: right;
+    font-size: 16px;
+
     .favorite,
     .destroy {
+      display: inline-block;
+      margin: 8px;
       cursor: pointer;
     }
   }
 
-  .avatar {
-  }
-  .text {
-    margin-top: $margin/2;
+  .name {
+    margin-top: 32px;
   }
 }
 </style>
