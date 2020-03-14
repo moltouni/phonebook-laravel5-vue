afe53e8a74ad22f4f0b0516ba5105f3be8ec2ce4
Mario <marionovak5@gmail.com>
2020-03-14 04:02:12 +0100

    Frontend build fixes: browsersync, package-lock

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,4 +15,4 @@ mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
    .sass('resources/assets/sass/login.scss', 'public/css');
 
-mix.browserSync({ proxy: 'phonebook.test' });
+mix.browserSync({ proxy: 'localhost' });
